92b1415e5c4ef34b4de145e2f9ae1765d3288538
Bloodcrane <92672973+Bloodcrane@users.noreply.github.com>
2024-03-20 04:21:59 +0400

    3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { RouterProvider, HashRouter } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import router from './router';
 
 function App() {
   return (
     <div className='App'>
-      <RouterProvider router={HashRouter(router)}/>
+      <RouterProvider router={router}/>
     </div>
   )
 }
