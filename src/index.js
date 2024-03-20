26301ffce46b4894fe500f3b30c8e49dd47472a0
Bloodcrane <92672973+Bloodcrane@users.noreply.github.com>
2024-03-20 04:37:51 +0400

    uu

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 import { HashRouter } from 'react-router-dom';
 
 const root = createRoot(document.getElementById('root'));
-root.render( 
+root.render(
   <HashRouter>
     <App />
   </HashRouter>
